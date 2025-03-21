4aa8c8b6882b9ab43173c167523755b530de55ca
Colm <168563265+Hulupeep@users.noreply.github.com>
2025-03-21 08:59:53 +0000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = function(eleventyConfig) {
   return {
     dir: {
-      input: ".", // Tell Eleventy where to look for files.
-      output: "_site", // Keep this for consistency.
-      includes: "_includes", // Eleventy will look for includes here
-      layouts: "."   // Tell Eleventy to look for layouts in the root directory
+      input: ".",
+      output: "_site",
+      includes: "_includes",
+      layouts: "."
     }
   };
 };
