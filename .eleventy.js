5791296c7769d86db34692b9bc2a1a1a4c91aee2
Colm <168563265+Hulupeep@users.noreply.github.com>
2025-03-21 12:19:47 +0000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,18 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("content/*.png");
+  eleventyConfig.addPassthroughCopy("content/*.png"); // Ensure PNG images are copied
+  eleventyConfig.addPassthroughCopy("content/*.jpg"); // In case you switch formats
+  eleventyConfig.addPassthroughCopy("content/*.webp"); // Supports WebP for optimization
+
+  return {
+    dir: {
+      input: "content",
+      output: "_site",
+      includes: "../_includes",
+      layouts: "../_includes"
+    }
+  };
+};
+
   return {
     dir: {
       input: "content",       // Markdown files directory
