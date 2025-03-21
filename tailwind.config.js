0c8a43ee2f8140b07f44652c5f54088539c57b6f
Colm <168563265+Hulupeep@users.noreply.github.com>
2025-03-21 10:31:24 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./content/**/*.md", "./*.html"],
+  content: [
+    "./content/**/*.md",
+    "./_includes/**/*.html"  // <-- Add this to scan all templates
+  ],
   theme: {
     extend: {},
   },
